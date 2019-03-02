95179bb40c8e0d14e39dc8cfd621c7abdedb7195
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2019-03-01 20:13:23 -0700

    Trim trailing whitespace in stubs file

--- generate.sh
+++ generate.sh
@@ -14,3 +14,7 @@ FILE="wordpress-stubs.php"
 # Shim the global $wpdb declaration, since it's actually set up inside a
 # function call.
 echo $'\n/**\n * WordPress database abstraction object.\n * @var wpdb\n */\n$wpdb = null;' >> $FILE
+
+# Trim tailing whitespace.  Not using sed because it seemed to struggle with
+# some characters in the file.
+perl -i -lpe "s/[[:space:]]+$//g" $FILE
