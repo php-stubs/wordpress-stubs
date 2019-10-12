328f28a2c762be422abf87d387aa5f6b97878f43
Viktor Szépe <viktor@szepe.net>
2019-10-12 20:35:00 +0000

    Generate stubs for WordPress 5.2.2

--- wordpress-stubs.php
+++ wordpress-stubs.php
@@ -72894,6 +72894,26 @@ function wp_maybe_grant_resume_extensions_caps($allcaps)
 {
 }
 /**
+ * Filters the user capabilities to grant the 'view_site_health_checks' capabilities as necessary.
+ *
+ * @since 5.2.2
+ *
+ * @param bool[]   $allcaps An array of all the user's capabilities.
+ * @param string[] $caps    Required primitive capabilities for the requested capability.
+ * @param array    $args {
+ *     Arguments that accompany the requested capability check.
+ *
+ *     @type string    $0 Requested capability.
+ *     @type int       $1 Concerned user ID.
+ *     @type mixed  ...$2 Optional second and further parameters, typically object ID.
+ * }
+ * @param WP_User  $user    The user object.
+ * @return bool[] Filtered array of the user's capabilities.
+ */
+function wp_maybe_grant_site_health_caps($allcaps, $caps, $args, $user)
+{
+}
+/**
  * Taxonomy API: Core category-specific template tags
  *
  * @package WordPress
