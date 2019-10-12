a64715d13dfb81eb87b5e610baa002247300e167
Viktor Szépe <viktor@szepe.net>
2019-10-12 20:35:31 +0000

    Generate stubs for WordPress 5.2.3

--- wordpress-stubs.php
+++ wordpress-stubs.php
@@ -96913,18 +96913,6 @@ function get_the_content($more_link_text = \null, $strip_teaser = \false, $post 
 {
 }
 /**
- * Preview fix for JavaScript bug with foreign languages.
- *
- * @since 3.1.0
- * @access private
- *
- * @param array $match Match array from preg_replace_callback.
- * @return string
- */
-function _convert_urlencoded_to_entities($match)
-{
-}
-/**
  * Display the post excerpt.
  *
  * @since 0.71
