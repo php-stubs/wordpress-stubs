7a48e8ab66db2cba66dd3f546b3bc0171803b59a
Viktor Szépe <viktor@szepe.net>
2019-10-17 16:27:29 +0000

    More robust updater

--- update-versions.sh
+++ update-versions.sh
@@ -4,10 +4,15 @@ set -e
 
 CORE_JSON="$(wget -q -O- "https://packagist.org/packages/johnpbloch/wordpress-core.json")"
 
-for V in 4.7  4.8  4.9  5.0  5.1  5.2; do
+for V in 4.7  4.8  4.9  5.0  5.1  5.2  5.3; do
     # Find latest version
     printf -v JQ_QUERY '.package.versions[].version | select(test("^%s\\\\.%s\\\\.\\\\d+$"))' "${V%.*}" "${V#*.}"
     LATEST="$(jq -r "$JQ_QUERY" <<<"$CORE_JSON" | sort -t "." -k 3 -g | tail -n 1)"
+    if [ -z "$LATEST" ]; then
+        echo "No version for ${V}!"
+        continue;
+    fi
+
     echo "Releasing ${LATEST} version ..."
 
     if git rev-parse "refs/tags/v${LATEST}" >/dev/null 2>&1; then
