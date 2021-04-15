fad7afad78b4a92b702a1570edf6dd1c9d9373cd
Viktor Szépe <viktor@szepe.net>
2021-04-15 14:46:15 +0000

    Generate stubs for WordPress 5.6.1

--- release-latest-versions.sh
+++ release-latest-versions.sh
@@ -4,28 +4,20 @@
 
 set -e
 
-CORE_JSON="$(wget -q -O- "https://packagist.org/packages/johnpbloch/wordpress-core.json")"
+Do_release()
+{
+    local VERSION="$1"
+    local SED_EXP
 
-# @TODO Use branches!
-for V in                             4.7  4.8  4.9 \
-         5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7; do
-    # Find latest version
-    printf -v JQ_FILTER '.package.versions[].version | select(test("^%s\\\\.%s\\\\.\\\\d+$"))' "${V%.*}" "${V#*.}"
-    LATEST="$(jq -r "$JQ_FILTER" <<<"$CORE_JSON" | sort -t "." -k 3 -g | tail -n 1)"
-    if [ -z "$LATEST" ]; then
-        echo "No version for ${V}!"
-        continue;
-    fi
+    echo "Releasing ${VERSION} version ..."
 
-    echo "Releasing ${LATEST} version ..."
-
-    if git rev-parse "refs/tags/v${LATEST}" >/dev/null 2>&1; then
+    if git rev-parse "refs/tags/v${VERSION}" >/dev/null 2>&1; then
         echo "Tag exists!"
-        continue;
+        return;
     fi
 
     # Get new version
-    printf -v SED_EXP 's#\\("johnpbloch/wordpress"\\): "[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+"#\\1: "%s"#' "$LATEST"
+    printf -v SED_EXP 's#\\("johnpbloch/wordpress"\\): "[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+"#\\1: "%s"#' "${VERSION}"
     sed -i -e "$SED_EXP" source/composer.json
     composer run-script post-install-cmd
 
@@ -34,6 +26,25 @@ for V in                             4.7  4.8  4.9 \
     ./generate.sh
 
     # Tag version
-    git commit --all -m "Generate stubs for WordPress ${LATEST}"
-    git tag "v${LATEST}"
+    git commit --all -m "Generate stubs for WordPress ${VERSION}"
+    git tag "v${VERSION}"
+}
+
+CORE_JSON="$(wget -q -O- "https://packagist.org/packages/johnpbloch/wordpress-core.json")"
+
+# @TODO Use branches!
+for MINOR in                         4.7 4.8 4.9 \
+         5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7 5.8 5.9; do
+    # Find latest version
+    printf -v JQ_FILTER '.package.versions[].version | select(test("^%s\\\\.%s\\\\.\\\\d+$"))' "${MINOR%.*}" "${MINOR#*.}"
+    LATEST_FIVE="$(jq -r "$JQ_FILTER" <<<"$CORE_JSON" | sort -t "." -k 3 -g | tail -n 5)"
+
+    if [ -z "${LATEST_FIVE}" ]; then
+        echo "No version for ${MINOR}!"
+        continue;
+    fi
+
+    while read -r PATCH; do
+        Do_release "${PATCH}"
+    done <<<"${LATEST_FIVE}"
 done
