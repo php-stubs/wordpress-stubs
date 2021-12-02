04206ad5d84f7d39247988e528a9010905a8f962
John Blackbourn <john@humanmade.com>
2021-12-02 21:48:14 +0100

    Split this up a bit.

--- visitor.php
+++ visitor.php
@@ -2,6 +2,7 @@
 
 declare(strict_types = 1);
 
+use phpDocumentor\Reflection\DocBlock\Tags\Param;
 use PhpParser\Comment\Doc;
 use PhpParser\Node;
 use PhpParser\Node\Stmt\ClassMethod;
@@ -50,7 +51,7 @@ return new class extends NodeVisitor {
             return null;
         }
 
-        /** @var phpDocumentor\Reflection\DocBlock\Tags\Param[] */
+        /** @var \phpDocumentor\Reflection\DocBlock\Tags\Param[] */
         $params = $docblock->getTagsByName('param');
 
         if (!$params) {
@@ -60,62 +61,11 @@ return new class extends NodeVisitor {
         $additions = [];
 
         foreach ($params as $param) {
-            $paramDescription = $param->getDescription()->__toString();
-            $paramVariableName = $param->getVariableName();
-            $paramVariableType = $param->getType();
+            $addition = $this->getAdditionFromParam($param);
 
-            // Skip if the parameter variable name or type are missing.
-            if (!$paramVariableName || !$paramVariableType) {
-                continue;
+            if ($addition !== null) {
+                $additions[] = $addition;
             }
-
-            // Skip if the description doesn't contain at least one correctly
-            // formatted `@type`, which indicates an array hash.
-            if (strpos($paramDescription, '    @type') === false) {
-                continue;
-            }
-
-            // Populate `$types` with the value of each top level `@type`.
-            $types = preg_split('/\R+    @type /', $paramDescription);
-            unset($types[0]);
-            $elements = [];
-
-            // PHPStan dosn't support typed array shapes (`int[]{...}`) so replace
-            // typed arrays such as `int[]` with `array`.
-            $paramVariableType = preg_replace('#[a-zA-Z0-9_]+\[\]#', 'array', $paramVariableType->__toString());
-
-            if (strpos($paramVariableType, 'array') === false) {
-                // Skip if we have hash notation that's not for an array (ie. for `object`).
-                continue;
-            }
-
-            if (strpos($paramVariableType, 'array|') !== false) {
-                // Move `array` to the end of union types so the appended array shape works.
-                $paramVariableType = str_replace('array|', '', $paramVariableType) . '|array';
-            }
-
-            foreach ($types as $typeTag) {
-                list($type, $name) = preg_split('#\s+#', trim($typeTag));
-
-                // Bail out completely if any element doesn't have a static key.
-                if (strpos($name, '...$') !== false) {
-                    return null;
-                }
-
-                // Bail out completely if the name of any element is invalid.
-                if (strpos($name, '$') !== 0) {
-                    return null;
-                }
-
-                $elements[] = substr($name, 1) . '?: ' . $type;
-            }
-
-            $additions[] = sprintf(
-                " * @phpstan-param %1\$s{\n *   %2\$s,\n * } $%3\$s",
-                str_replace(['|string', 'string|'], '', $paramVariableType),
-                implode(",\n *   ", $elements),
-                $paramVariableName
-            );
         }
 
         if (!$additions) {
@@ -129,5 +79,65 @@ return new class extends NodeVisitor {
         );
 
         return new Doc($newDocComment, $docComment->getLine(), $docComment->getFilePos());
+    }
+
+    private function getAdditionFromParam(Param $param): ?string
+    {
+        $paramDescription = $param->getDescription()->__toString();
+        $paramVariableName = $param->getVariableName();
+        $paramVariableType = $param->getType();
+
+        // Skip if the parameter variable name or type are missing.
+        if (!$paramVariableName || !$paramVariableType) {
+            return null;
+        }
+
+        // Skip if the description doesn't contain at least one correctly
+        // formatted `@type`, which indicates an array hash.
+        if (strpos($paramDescription, '    @type') === false) {
+            return null;
+        }
+
+        // Populate `$types` with the value of each top level `@type`.
+        $types = preg_split('/\R+    @type /', $paramDescription);
+        unset($types[0]);
+        $elements = [];
+
+        // PHPStan dosn't support typed array shapes (`int[]{...}`) so replace
+        // typed arrays such as `int[]` with `array`.
+        $paramVariableType = preg_replace('#[a-zA-Z0-9_]+\[\]#', 'array', $paramVariableType->__toString());
+
+        if (strpos($paramVariableType, 'array') === false) {
+            // Skip if we have hash notation that's not for an array (ie. for `object`).
+            return null;
+        }
+
+        if (strpos($paramVariableType, 'array|') !== false) {
+            // Move `array` to the end of union types so the appended array shape works.
+            $paramVariableType = str_replace('array|', '', $paramVariableType) . '|array';
+        }
+
+        foreach ($types as $typeTag) {
+            list($type, $name) = preg_split('#\s+#', trim($typeTag));
+
+            // Bail out completely if any element doesn't have a static key.
+            if (strpos($name, '...$') !== false) {
+                return null;
+            }
+
+            // Bail out completely if the name of any element is invalid.
+            if (strpos($name, '$') !== 0) {
+                return null;
+            }
+
+            $elements[] = substr($name, 1) . '?: ' . $type;
+        }
+
+        return sprintf(
+            " * @phpstan-param %1\$s{\n *   %2\$s,\n * } $%3\$s",
+            str_replace(['|string', 'string|'], '', $paramVariableType),
+            implode(",\n *   ", $elements),
+            $paramVariableName
+        );
     }
 };
