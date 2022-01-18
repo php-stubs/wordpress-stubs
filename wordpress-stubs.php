c231fd166b04a448d739ab464de6992f729f6498
Pascal Birchler <pascalb@google.com>
2022-01-18 13:49:19 +0100

    Update stubs for WordPress 5.8.3

--- wordpress-stubs.php
+++ wordpress-stubs.php
@@ -93161,12 +93161,14 @@ namespace {
      * Encode the Unicode values to be used in the URI.
      *
      * @since 1.5.0
+     * @since 5.8.3 Added the `encode_ascii_characters` parameter.
      *
-     * @param string $utf8_string
-     * @param int    $length Max  length of the string
+     * @param string $utf8_string             String to encode.
+     * @param int    $length                  Max length of the string
+     * @param bool   $encode_ascii_characters Whether to encode ascii characters such as < " '
      * @return string String with Unicode encoded for URI.
      */
-    function utf8_uri_encode($utf8_string, $length = 0)
+    function utf8_uri_encode($utf8_string, $length = 0, $encode_ascii_characters = \false)
     {
     }
     /**
