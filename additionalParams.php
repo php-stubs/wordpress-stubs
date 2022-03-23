4bfb6fb3dd8862fc6df7a908d41e22e020e7bf28
John Blackbourn <john@humanmade.com>
2022-03-23 22:02:04 +0000

    No need to override these parameters.

--- additionalParams.php
+++ additionalParams.php
@@ -3,8 +3,8 @@
 $httpReturnType = 'array{headers:Requests_Utility_CaseInsensitiveDictionary,body:string,response:array{code:int,message:string},cookies:array<int,WP_HTTP_Cookie>,filename:string|null,http_response:WP_HTTP_Requests_Response}|WP_Error';
 
 return [
-    'wp_remote_get' => [$httpReturnType, 'url'=>'string', 'args'=>'array'],
-    'wp_remote_head' => [$httpReturnType, 'url'=>'string', 'args'=>'array'],
-    'wp_remote_post' => [$httpReturnType, 'url'=>'string', 'args'=>'array'],
-    'wp_remote_request' => [$httpReturnType, 'url'=>'string', 'args'=>'array'],
+    'wp_remote_get' => [$httpReturnType],
+    'wp_remote_head' => [$httpReturnType],
+    'wp_remote_post' => [$httpReturnType],
+    'wp_remote_request' => [$httpReturnType],
 ];
