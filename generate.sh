db98ee545ae38bf5dbd2f9d3aacd583066f02821
Viktor Szépe <viktor@szepe.net>
2022-07-05 10:57:59 +0000

    No literal-string for $query

--- generate.sh
+++ generate.sh
@@ -26,8 +26,8 @@ fi
     --traits \
     --out="$FILE"
 
-# Use literal-string type for wpdb::prepare() query statement parameter.
-sed -i -e 's#^.*@param string \+\$query \+Query statement.*$#&\n         * @phpstan-param literal-string $query#' "$FILE"
+## Use literal-string type for wpdb::prepare() query statement parameter.
+#sed -i -e 's#^.*@param string \+\$query \+Query statement.*$#&\n         * @phpstan-param literal-string $query#' "$FILE"
 
 # Shim the global $wpdb declaration, since it's actually set up inside a function call.
 if grep -qFx 'namespace {' "$FILE"; then
