5e4b812639f3a067e2f05ca9d71dd58966d0158a
Viktor Szépe <viktor@szepe.net>
2022-07-20 11:25:39 +0000

    Add ReturnTypeWillChange for PHP 8.1
    From  #41

---
+++ returnTypeWillChangeMap.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'Requests_Cookie_Jar::getIterator',
+    'Requests_Cookie_Jar::offsetExists',
+    'Requests_Cookie_Jar::offsetGet',
+    'Requests_Cookie_Jar::offsetSet',
+    'Requests_Cookie_Jar::offsetUnset',
+    'Requests_Response_Headers::getIterator',
+    'Requests_Response_Headers::offsetGet',
+    'Requests_Response_Headers::offsetSet',
+    'Requests_Utility_CaseInsensitiveDictionary::getIterator',
+    'Requests_Utility_CaseInsensitiveDictionary::offsetExists',
+    'Requests_Utility_CaseInsensitiveDictionary::offsetGet',
+    'Requests_Utility_CaseInsensitiveDictionary::offsetSet',
+    'Requests_Utility_CaseInsensitiveDictionary::offsetUnset',
+    'Requests_Utility_FilteredIterator::__unserialize',
+    'Requests_Utility_FilteredIterator::current',
+    'Requests_Utility_FilteredIterator::unserialize',
+];
