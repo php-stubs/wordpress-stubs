d546c7c5f549aa17c7fc2225f4ec31662325a9ba
Jan Willem Oostendorp <webmaster@janw.me>
2022-09-30 17:16:38 +0200

    Add extra php attribute to methods for 8+
    These functions are giving notices on php 8+.
    The fix is adding a extra php attribute to the methods: #[ReturnTypeWillChange]

--- generate.sh
+++ generate.sh
@@ -35,3 +35,36 @@ if grep -qFx 'namespace {' "$FILE"; then
 else
     printf '\n/**\n * WordPress database abstraction object.\n * @var wpdb\n */\n$wpdb = \\null;\n' >>"$FILE"
 fi
+
+# These are function that need a extra attribute to prevent php8+ notices.
+declare -a PHP_FUNCTIONS=(
+    'Requests_Utility_FilteredIterator::unserialize'
+    'Requests_Utility_FilteredIterator::__unserialize'
+    'Requests_Utility_FilteredIterator::current'
+    'Requests_Cookie_Jar::offsetExists'
+    'Requests_Cookie_Jar::offsetGet'
+    'Requests_Cookie_Jar::offsetSet'
+    'Requests_Cookie_Jar::offsetUnset'
+    'Requests_Cookie_Jar::getIterator'
+    'Requests_Utility_CaseInsensitiveDictionary::offsetExists'
+    'Requests_Utility_CaseInsensitiveDictionary::offsetGet'
+    'Requests_Utility_CaseInsensitiveDictionary::offsetSet'
+    'Requests_Utility_CaseInsensitiveDictionary::offsetUnset'
+    'Requests_Utility_CaseInsensitiveDictionary::getIterator'
+)
+for PHP_FUNCTION in "${PHP_FUNCTIONS[@]}"; do
+    # Get the line where the method is defined.
+    LINE=$(php -r "include 'wordpress-stubs.php'; print (new ReflectionMethod('${PHP_FUNCTION}'))->getStartLine();")
+    echo "${PHP_FUNCTION} is defined on line ${LINE}"
+
+    # Check the line above for #[ReturnTypeWillChange]
+    if [[ $(sed "$((${LINE}-1))q;d" ${FILE}) == *'#[ReturnTypeWillChange]' ]]; then
+        echo "${PHP_FUNCTION} already has #[ReturnTypeWillChange]"
+        continue # Already there.
+    fi
+
+    # Grab the current leading whitespace so we can keep the #[ReturnTypeWillChange] indented correctly.
+    LEADING_WHITESPACE="$(sed "${LINE}q;d" ${FILE} | sed "s#\([[:space:]]\+\)\(.*\)#\1#")"
+    # Insert the #[ReturnTypeWillChange]
+    sed -e "${LINE}i\\" -e "${LEADING_WHITESPACE}#[ReturnTypeWillChange]" -i ${FILE}
+done
