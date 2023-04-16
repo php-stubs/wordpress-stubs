ac44bd7145f59a24910a1395de68d67ffa47cf20
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2023-04-16 12:36:16 +0200

    Add get_page_by_path() to function map (#75)

--- functionMap.php
+++ functionMap.php
@@ -82,6 +82,7 @@ return [
     'get_object_taxonomies' => ["(\$output is 'names' ? array<int, string> : array<int, \WP_Taxonomy>)"],
     'get_comment' => ["(\$output is 'ARRAY_A' ? array<string, mixed>|null : (\$output is 'ARRAY_N' ? array<int, mixed>|null : \WP_Comment|null))"],
     'get_post' => ["(\$output is 'ARRAY_A' ? array<string, mixed>|null : (\$output is 'ARRAY_N' ? array<int, mixed>|null : \WP_Post|null))"],
+    'get_page_by_path' => ["(\$output is 'ARRAY_A' ? array<string, mixed>|null : (\$output is 'ARRAY_N' ? array<int, mixed>|null : \WP_Post|null))"],
     'has_action' => ['($callback is false ? bool : bool|int)'],
     'has_filter' => ['($callback is false ? bool : bool|int)'],
 ];
