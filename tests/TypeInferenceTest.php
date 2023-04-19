6d0dd2dd70167e8046bb6f7d1fa1d8347ac71ff8
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2023-04-19 20:51:35 +0200

    Add tests for WP_Theme::offsetExists() and WP_Theme::offsetGet()

--- tests/TypeInferenceTest.php
+++ tests/TypeInferenceTest.php
@@ -18,6 +18,7 @@ class TypeInferenceTest extends \PHPStan\Testing\TypeInferenceTestCase
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_taxonomies.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/has_filter.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/mysql2date.php');
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_theme.php');
     }
 
     /**
