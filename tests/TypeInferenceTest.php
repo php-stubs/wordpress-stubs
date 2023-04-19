f96efb5230a518de6c6ce54411abd8ca2c11d4cb
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2023-04-19 20:06:47 +0200

    Add tests for get_page_by_path()

--- tests/TypeInferenceTest.php
+++ tests/TypeInferenceTest.php
@@ -13,6 +13,7 @@ class TypeInferenceTest extends \PHPStan\Testing\TypeInferenceTestCase
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_comment.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_object_taxonomies.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_post.php');
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/get_page_by_path.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/get_taxonomies.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/has_filter.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/mysql2date.php');
