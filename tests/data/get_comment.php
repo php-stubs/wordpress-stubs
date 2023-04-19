10a61f03f2f10ba9eb7aa1439943acdd5f240218
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2023-04-19 20:05:15 +0200

    Fix tests for get_comment()

--- tests/data/get_comment.php
+++ tests/data/get_comment.php
@@ -15,14 +15,14 @@ assertType('WP_Comment|null', get_comment());
 assertType('WP_Comment|null', get_comment($comment));
 assertType('WP_Comment|null', get_comment($comment, OBJECT));
 
-// Unexpected output
-assertType('WP_Comment|null', get_comment($comment, 'Hello'));
-
-// Unknown output
-assertType('array|WP_Comment|null', get_comment($comment, $_GET['foo']));
-
 // Associative array output
 assertType('array<string, mixed>|null', get_comment($comment, ARRAY_A));
 
 // Numeric array output
 assertType('array<int, mixed>|null', get_comment($comment, ARRAY_N));
+
+// Unknown output
+assertType('array<int|string, mixed>|WP_Comment|null', get_comment($comment, (string)$_GET['unknown_string']));
+
+// Unexpected output
+assertType('WP_Comment|null', get_comment($comment, 'Hello'));
