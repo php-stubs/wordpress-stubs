e53228ea81386cd9332127da9cb5a05e3d8dcc34
Mat Lipe <mat@matlipe.com>
2023-09-19 14:33:35 -0400

    Add dynamic return type for the get_term_by function (#105)
    - Return `array<int|string>` if `$output = 'ARRAY_N'`.
    - Return `array<string, int|string>` if default or `$output = 'ARRAY_A'`.
    - Return `WP_Term` if default or `$output = 'OBJECT'`.
    
    https://developer.wordpress.org/reference/functions/get_term_by/#parameters

---
+++ tests/data/get_term_by.php
@@ -0,0 +1,13 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace PhpStubs\WordPress\Core\Tests;
+
+use function get_term_by;
+use function PHPStan\Testing\assertType;
+
+assertType( 'WP_Error|WP_Term|false', get_term_by( 'term_id', 2, '', OBJECT ) );
+assertType( 'WP_Error|WP_Term|false', get_term_by( 'slug', 'test' ) );
+assertType( 'array<string, int|string>|WP_Error|false', get_term_by( 'term_id', 2, '', ARRAY_A ) );
+assertType( 'array<int, int|string>|WP_Error|false', get_term_by( 'term_id', 2, '', ARRAY_N ) );
