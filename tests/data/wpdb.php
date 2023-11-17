9979740755c89718f6fdafaedb6c06fe41857d39
Martin Herndl <martin@herndl.org>
2023-11-17 13:26:09 +0100

    Fix wpdb::get_results() return type
    Based on https://github.com/php-stubs/wordpress-stubs/pull/145

--- tests/data/wpdb.php
+++ tests/data/wpdb.php
@@ -14,8 +14,19 @@ assertType('array<string, mixed>|void|null', wpdb::get_row(null, 'ARRAY_A'));
 assertType('array<int, mixed>|void|null', wpdb::get_row(null, 'ARRAY_N'));
 
 // wpdb::get_results()
-assertType('stdClass|null', wpdb::get_results());
-assertType('stdClass|null', wpdb::get_results(null, 'OBJECT'));
-assertType('array<string, stdClass>|null', wpdb::get_results(null, 'OBJECT_K'));
-assertType('array<string, mixed>|null', wpdb::get_results(null, 'ARRAY_A'));
-assertType('array<int, mixed>|null', wpdb::get_results(null, 'ARRAY_N'));
+/*
+ *                       Any of ARRAY_A | ARRAY_N | OBJECT | OBJECT_K constants.
+ *                       ARRAY_A | ARRAY_N | OBJECT: return an array of rows indexed from 0 by SQL result row number.
+ *
+ *                       ARRAY_A: Each row is an associative array (column => value, ...),
+ *                       ARRAY_N: a numerically indexed array (0 => value, ...),
+ *                       OBJECT: an object ( ->column = value ), respectively.
+ *
+ *                       With OBJECT_K: return an associative array of row objects keyed by the value
+ *                       of each row's first column's value.
+ */
+assertType('array<int, array>|null', wpdb::get_results(null, 'ARRAY_A'));
+assertType('array<int, array<int, mixed>>|null', wpdb::get_results(null, 'ARRAY_N'));
+assertType('array<int, stdClass>|null', wpdb::get_results());
+assertType('array<int, stdClass>|null', wpdb::get_results(null, 'OBJECT'));
+assertType('array<stdClass>|null', wpdb::get_results(null, 'OBJECT_K'));
