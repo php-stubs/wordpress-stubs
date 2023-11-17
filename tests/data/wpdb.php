6107fdab6959d2883644540643e977cebe78a38d
Martin Herndl <martin@herndl.org>
2023-11-17 15:16:10 +0100

    Fix wpdb::get_results() return type (#146)
    * Fix wpdb::get_results() return type
    
    Based on https://github.com/php-stubs/wordpress-stubs/pull/145
    
    * Remove copy of WordPress docs from tests
    
    Co-authored-by: Viktor Szépe <viktor@szepe.net>
    
    * Improve wpdb::get_row()
    
    - use list type
    - don't specify string keys, since PHP might cast it to int
    
    ---------
    
    Co-authored-by: Viktor Szépe <viktor@szepe.net>

--- tests/data/wpdb.php
+++ tests/data/wpdb.php
@@ -10,12 +10,12 @@ use function PHPStan\Testing\assertType;
 // wpdb::get_row()
 assertType('stdClass|void|null', wpdb::get_row());
 assertType('stdClass|void|null', wpdb::get_row(null, 'OBJECT'));
-assertType('array<string, mixed>|void|null', wpdb::get_row(null, 'ARRAY_A'));
+assertType('array|void|null', wpdb::get_row(null, 'ARRAY_A'));
 assertType('array<int, mixed>|void|null', wpdb::get_row(null, 'ARRAY_N'));
 
 // wpdb::get_results()
-assertType('stdClass|null', wpdb::get_results());
-assertType('stdClass|null', wpdb::get_results(null, 'OBJECT'));
-assertType('array<string, stdClass>|null', wpdb::get_results(null, 'OBJECT_K'));
-assertType('array<string, mixed>|null', wpdb::get_results(null, 'ARRAY_A'));
-assertType('array<int, mixed>|null', wpdb::get_results(null, 'ARRAY_N'));
+assertType('array<int, array>|null', wpdb::get_results(null, 'ARRAY_A'));
+assertType('array<int, array<int, mixed>>|null', wpdb::get_results(null, 'ARRAY_N'));
+assertType('array<int, stdClass>|null', wpdb::get_results());
+assertType('array<int, stdClass>|null', wpdb::get_results(null, 'OBJECT'));
+assertType('array<stdClass>|null', wpdb::get_results(null, 'OBJECT_K'));
