38f834c6d313dee03771595c694d5d1d419a79a9
Mat Lipe <mat@matlipe.com>
2023-12-04 23:34:20 +0000

    Improve return type for `get_post_types`
    The result of `get_post_type` is an associative array keyed with the post type
    slugs.

--- tests/data/get_post_types.php
+++ tests/data/get_post_types.php
@@ -8,14 +8,14 @@ use function get_post_types;
 use function PHPStan\Testing\assertType;
 
 // Default output
-assertType('array<int, string>', get_post_types([]));
-assertType('array<int, string>', get_post_types([], 'names'));
+assertType('array<string, string>', get_post_types([]));
+assertType('array<string, string>', get_post_types([], 'names'));
 
 // Objects output
-assertType('array<int, WP_Post_Type>', get_post_types([], 'objects'));
+assertType('array<string, WP_Post_Type>', get_post_types([], 'objects'));
 
 // Unknown string
-assertType('array<int, string|WP_Post_Type>', get_post_types([], (string)$_GET['unknown_string']));
+assertType('array<string, string|WP_Post_Type>', get_post_types([], (string)$_GET['unknown_string']));
 
 // Unexpected output
-assertType('array<int, WP_Post_Type>', get_post_types([], 'Hello'));
+assertType('array<string, WP_Post_Type>', get_post_types([], 'Hello'));
