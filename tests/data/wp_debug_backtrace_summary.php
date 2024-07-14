a9a460a3192ceb647b345fd00da2417ee65d2200
Marian <42134098+IanDelMar@users.noreply.github.com>
2024-07-14 16:59:36 +0200

    bump PHP version for dev

--- tests/data/wp_debug_backtrace_summary.php
+++ tests/data/wp_debug_backtrace_summary.php
@@ -8,5 +8,5 @@ use function wp_debug_backtrace_summary;
 use function PHPStan\Testing\assertType;
 
 assertType('string', wp_debug_backtrace_summary());
-assertType('string', wp_debug_backtrace_summary( null, 0, true ));
-assertType('list<string>', wp_debug_backtrace_summary( null, 0, false ));
+assertType('string', wp_debug_backtrace_summary(null, 0, true));
+assertType('list<string>', wp_debug_backtrace_summary(null, 0, false));
