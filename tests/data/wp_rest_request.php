7b564939560af5d958ae807f2a1f8dfb73e91d01
Marian <42134098+IanDelMar@users.noreply.github.com>
2024-07-14 16:18:59 +0200

    add tests for general array type

--- tests/data/wp_rest_request.php
+++ tests/data/wp_rest_request.php
@@ -7,6 +7,19 @@ namespace PhpStubs\WordPress\Core\Tests;
 use function PHPStan\Testing\assertType;
 
 /**
+ * @var \WP_REST_Request<array> $request
+ */
+$request = new WP_REST_Request();
+
+assertType('mixed', $request->get_param('maybeParam'));
+
+assertType('mixed', $request['maybeParam']);
+
+assertType('array', $request->get_params());
+
+assertType('bool', $request->has_param('maybeParam'));
+
+/**
  * @var \WP_REST_Request<array{
  *      stringParam: string,
  *      intParam: int,
