8b6e534c708e88b394c8177d046f61bc898d1835
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2024-07-14 18:25:31 +0200

    Fix parameter name for `WP_REST_Request::has_param()` in functionMap (#188)

--- tests/data/wp_rest_request.php
+++ tests/data/wp_rest_request.php
@@ -7,6 +7,19 @@ namespace PhpStubs\WordPress\Core\Tests;
 use function PHPStan\Testing\assertType;
 
 /**
+ * @var \WP_REST_Request<array> $request
+ */
+$request = new WP_REST_Request();
+
+assertType('mixed', $request->get_param('maybeParam'));
+
+assertType('mixed', $request['maybeParam']);
+
+assertType('array', $request->get_params());
+
+assertType('bool', $request->has_param('maybeParam'));
+
+/**
  * @var \WP_REST_Request<array{
  *      stringParam: string,
  *      intParam: int,
@@ -18,16 +31,16 @@ $request = new WP_REST_Request();
 assertType('string', $request->get_param('stringParam'));
 assertType('int', $request->get_param('intParam'));
 assertType('bool', $request->get_param('boolParam'));
-assertType('null', $request->get_param('unknownParam'));
+assertType('null', $request->get_param('nonExistentParam'));
 
 assertType('string', $request['stringParam']);
 assertType('int', $request['intParam']);
 assertType('bool', $request['boolParam']);
-assertType('null', $request['unknownParam']);
+assertType('null', $request['nonExistentParam']);
 
 assertType('array{stringParam: string, intParam: int, boolParam: bool}', $request->get_params());
 
 assertType('bool', $request->has_param('stringParam'));
 assertType('bool', $request->has_param('intParam'));
 assertType('bool', $request->has_param('boolParam'));
-assertType('bool', $request->has_param('unknownParam'));
+assertType('bool', $request->has_param('nonExistentParam'));
