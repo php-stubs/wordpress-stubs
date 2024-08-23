a3952c3ab2f6b02d37aa9e18985b6710fb3ba701
Marian <42134098+IanDelMar@users.noreply.github.com>
2024-08-23 18:25:23 +0200

    fix coding standards

--- tests/data/get_category_by_path.php
+++ tests/data/get_category_by_path.php
@@ -10,7 +10,7 @@ use function PHPStan\Testing\assertType;
 /** @var bool $bool */
 $bool;
 
-assertType('WP_Error|WP_Term|null', get_category_by_path('', $bool, ));
+assertType('WP_Error|WP_Term|null', get_category_by_path('', $bool));
 assertType('WP_Error|WP_Term|null', get_category_by_path('', $bool, 'OBJECT'));
 assertType('array<string, mixed>|WP_Error|null', get_category_by_path('', $bool, 'ARRAY_A'));
 assertType('array<int, mixed>|WP_Error|null', get_category_by_path('', $bool, 'ARRAY_N'));
