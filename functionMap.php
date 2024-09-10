90de094903be1a27260d634ddc0769af61f4963a
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2024-09-10 13:35:40 +0200

    Add __get and __call for WP_Query (#220)
    * Add __get and __call for WP_Query
    
    * Fix wp_query test
    
    * Add note about void->null conversion
    
    * Fix cs
    
    * Fix cs

--- functionMap.php
+++ functionMap.php
@@ -192,4 +192,5 @@ return [
     'size_format' => ["(\$bytes is not numeric ? false : (\$bytes is negative-int|'0' ? false : string))"],
     'WP_Translations::translate' => ['($singular is null ? null : string)'],
     'WP_Translations::translate_plural' => ['($singular is null ? null : ($plural is null ? T : string))', '@phpstan-template T' => 'of string|null', 'singular' => 'T', 'count' => 'int'],
+    'WP_Query' => [null, '@phpstan-property-read bool $query_vars_changed' => '', '@phpstan-property-read bool|string $query_vars_hash' => '', '@phpstan-method void init_query_flags()' => ''],
 ];
