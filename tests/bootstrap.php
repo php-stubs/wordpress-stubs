e5fee80b90ec347fc8b8c1b94ffc79770a2fde16
Marian <42134098+IanDelMar@users.noreply.github.com>
2024-09-30 02:22:17 +0200

    Update name of disabled phpcs rule

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PhpStubs\WordPress\Core\Tests;
 
-// phpcs:disable Squiz.PHP.DiscouragedFunctions
+// phpcs:disable Generic.PHP.ForbiddenFunctions.Found
 define('OBJECT', 'OBJECT');
 define('ARRAY_A', 'ARRAY_A');
 define('ARRAY_N', 'ARRAY_N');
