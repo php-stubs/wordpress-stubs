4215210ec9cc78558e735d64278aeb411dd26ec5
Marian <42134098+IanDelMar@users.noreply.github.com>
2024-11-12 04:17:42 +0100

    Fix return type of _get_list_table()

--- tests/data/_get_list_table.php
+++ tests/data/_get_list_table.php
@@ -7,8 +7,16 @@ namespace PhpStubs\WordPress\Core\Tests;
 use function _get_list_table;
 use function PHPStan\Testing\assertType;
 
-assertType('false', _get_list_table('Not_WP_List_Table'));
+// A non-class-string
+assertType('false', _get_list_table('string'));
 
+// A class-string that is not class-string<WP_List_Table>
+assertType('false', _get_list_table('WP_Post'));
+
+// WP_List_Table itself
+assertType('false', _get_list_table('WP_List_Table'));
+
+// Core WP_List_Table classes
 assertType('WP_Posts_List_Table', _get_list_table('WP_Posts_List_Table'));
 assertType('WP_Media_List_Table', _get_list_table('WP_Media_List_Table'));
 assertType('WP_Terms_List_Table', _get_list_table('WP_Terms_List_Table'));
@@ -18,7 +26,6 @@ assertType('WP_Post_Comments_List_Table', _get_list_table('WP_Post_Comments_List
 assertType('WP_Links_List_Table', _get_list_table('WP_Links_List_Table'));
 assertType('WP_Plugin_Install_List_Table', _get_list_table('WP_Plugin_Install_List_Table'));
 assertType('WP_Themes_List_Table', _get_list_table('WP_Themes_List_Table'));
-assertType('WP_Theme_Install_List_Table', _get_list_table('WP_Theme_Install_List_Table'));
 assertType('WP_Plugins_List_Table', _get_list_table('WP_Plugins_List_Table'));
 assertType('WP_Application_Passwords_List_Table', _get_list_table('WP_Application_Passwords_List_Table'));
 assertType('WP_MS_Sites_List_Table', _get_list_table('WP_MS_Sites_List_Table'));
@@ -26,3 +33,18 @@ assertType('WP_MS_Users_List_Table', _get_list_table('WP_MS_Users_List_Table'));
 assertType('WP_MS_Themes_List_Table', _get_list_table('WP_MS_Themes_List_Table'));
 assertType('WP_Privacy_Data_Export_Requests_List_Table', _get_list_table('WP_Privacy_Data_Export_Requests_List_Table'));
 assertType('WP_Privacy_Data_Removal_Requests_List_Table', _get_list_table('WP_Privacy_Data_Removal_Requests_List_Table'));
+
+// Union of core WP_List_Table classes
+assertType('WP_Media_List_Table|WP_Posts_List_Table', _get_list_table(isset($_GET['foo']) ? 'WP_Posts_List_Table' : 'WP_Media_List_Table'));
+
+// Union of core WP_List_Table class and class that is not a subclass of WP_List_Table
+assertType('WP_Posts_List_Table|false', _get_list_table(isset($_GET['foo']) ? 'WP_Posts_List_Table' : 'WP_Post'));
+
+// WP_Post_Comments_List_Table is generalized WP_Comments_List_Table
+assertType('WP_Comments_List_Table', _get_list_table(isset($_GET['foo']) ? 'WP_Comments_List_Table' : 'WP_Post_Comments_List_Table'));
+
+// WP_Theme_Install_List_Table is generalized to WP_Themes_List_Table
+assertType('WP_Themes_List_Table', _get_list_table(isset($_GET['foo']) ? 'WP_Themes_List_Table' : 'WP_Theme_Install_List_Table'));
+
+// Unknown string
+assertType('WP_Application_Passwords_List_Table|WP_Comments_List_Table|WP_Links_List_Table|WP_Media_List_Table|WP_MS_Sites_List_Table|WP_MS_Themes_List_Table|WP_MS_Users_List_Table|WP_Plugin_Install_List_Table|WP_Plugins_List_Table|WP_Posts_List_Table|WP_Privacy_Data_Export_Requests_List_Table|WP_Privacy_Data_Removal_Requests_List_Table|WP_Terms_List_Table|WP_Themes_List_Table|WP_Users_List_Table|false', _get_list_table(Faker::string()));
