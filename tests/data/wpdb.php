17d6f1581e6fd1396bcc2f24072efd19cf5fffff
Viktor Szépe <viktor@szepe.net>
2025-02-12 06:05:01 +0100

    Update wpdb.php

--- tests/data/wpdb.php
+++ tests/data/wpdb.php
@@ -10,11 +10,11 @@ use function PHPStan\Testing\assertType;
 // wpdb::get_row()
 assertType('stdClass|null', wpdb::get_row());
 assertType('stdClass|null', wpdb::get_row(null, 'OBJECT'));
-assertType('array|null', wpdb::get_row(null, 'ARRAY_A'));
+assertType('array<mixed>|null', wpdb::get_row(null, 'ARRAY_A'));
 assertType('list<mixed>|null', wpdb::get_row(null, 'ARRAY_N'));
 
 // wpdb::get_results()
-assertType('list<array>|null', wpdb::get_results(null, 'ARRAY_A'));
+assertType('list<array<mixed>>|null', wpdb::get_results(null, 'ARRAY_A'));
 assertType('list<array<int, mixed>>|null', wpdb::get_results(null, 'ARRAY_N'));
 assertType('list<stdClass>|null', wpdb::get_results());
 assertType('list<stdClass>|null', wpdb::get_results(null, 'OBJECT'));
