e2fbd92199f23e30e997d1fbd656796ecd6e0a01
Viktor Szépe <viktor@szepe.net>
2025-02-12 06:02:34 +0100

    Update _wp_json_sanity_check.php

--- tests/data/_wp_json_sanity_check.php
+++ tests/data/_wp_json_sanity_check.php
@@ -11,6 +11,6 @@ assertType('null', _wp_json_sanity_check(null, 1));
 assertType('bool', _wp_json_sanity_check(Faker::bool(), 1));
 assertType('int', _wp_json_sanity_check(Faker::int(), 1));
 assertType('string', _wp_json_sanity_check(Faker::string(), 1));
-assertType('array', _wp_json_sanity_check(Faker::array(), 1));
+assertType('array<mixed>', _wp_json_sanity_check(Faker::array(), 1));
 assertType('stdClass', _wp_json_sanity_check(Faker::stdClass(), 1));
 assertType('mixed', _wp_json_sanity_check(Faker::mixed(), 1));
