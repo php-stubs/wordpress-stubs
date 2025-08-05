6f3e1bcbf9e68cbb6fe2d48727a296b68961ead3
Marian <42134098+IanDelMar@users.noreply.github.com>
2025-08-06 01:08:16 +0200

    Narrow return type of wp_unique_prefixed_id()

--- tests/data/wp_unique_id.php
+++ tests/data/wp_unique_id.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PhpStubs\WordPress\Core\Tests;
 
 use function wp_unique_id;
+use function wp_unique_prefixed_id;
 use function PHPStan\Testing\assertType;
 
 assertType('non-falsy-string&numeric-string', wp_unique_id());
@@ -14,3 +15,11 @@ assertType('non-falsy-string&numeric-string', wp_unique_id(Faker::numericString(
 
 assertType('non-falsy-string', wp_unique_id('string'));
 assertType('non-falsy-string', wp_unique_id(Faker::string()));
+
+assertType('non-falsy-string&numeric-string', wp_unique_prefixed_id());
+assertType('non-falsy-string&numeric-string', wp_unique_prefixed_id(''));
+assertType('non-falsy-string&numeric-string', wp_unique_prefixed_id('1'));
+assertType('non-falsy-string&numeric-string', wp_unique_prefixed_id(Faker::numericString()));
+
+assertType('non-falsy-string', wp_unique_prefixed_id('string'));
+assertType('non-falsy-string', wp_unique_prefixed_id(Faker::string()));
