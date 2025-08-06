b616b528200e3cbaed2e8d5ef709964847be072b
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2025-08-06 04:46:59 +0200

    Narrow return type of wp_get_speculation_rules_configuration() (#312)

--- tests/TypeInferenceTest.php
+++ tests/TypeInferenceTest.php
@@ -74,6 +74,7 @@ class TypeInferenceTest extends TypeInferenceTestCase
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_get_object_terms.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_get_script_tag.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_get_server_protocol.php');
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_get_speculation_rules_configuration.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_is_numeric_array.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_list_bookmarks.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_list_categories.php');
