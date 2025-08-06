7d86ab9845f8d18cc047ecb38969f8d410f24c10
Marian <42134098+IanDelMar@users.noreply.github.com>
2025-08-07 00:37:50 +0200

    Update get_available_post_statuses.php

--- tests/data/get_available_post_statuses.php
+++ tests/data/get_available_post_statuses.php
@@ -8,3 +8,7 @@ use function get_available_post_statuses;
 use function PHPStan\Testing\assertType;
 
 assertType('list<string>', get_available_post_statuses());
+assertType('list<string>', get_available_post_statuses(''));
+assertType('list<string>', get_available_post_statuses('post'));
+assertType('list<string>', get_available_post_statuses('postType'));
+assertType('list<string>', get_available_post_statuses(Faker::string()));
