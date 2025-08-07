952d485375994f5968d557985f0cb306e1d19891
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2025-08-07 20:44:11 +0200

    Add note on void-null conversion (#327)

--- tests/data/echo_parameter.php
+++ tests/data/echo_parameter.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Note:
+ * Starting from PHPStan 1.10.49, void types, including void in unions, are
+ * transformed into null.
+ *
+ * @link https://github.com/phpstan/phpstan-src/pull/2778
+ */
+
 declare(strict_types=1);
 
 namespace PhpStubs\WordPress\Core\Tests;
