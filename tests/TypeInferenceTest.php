98033c1c225c80a55c225d9552e3c1a2d3d98cf4
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2025-08-08 17:58:50 +0200

    Narrow return type of wp_is_post_revision() (#330)

--- tests/TypeInferenceTest.php
+++ tests/TypeInferenceTest.php
@@ -88,6 +88,7 @@ class TypeInferenceTest extends TypeInferenceTestCase
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_get_server_protocol.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_get_speculation_rules_configuration.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_is_numeric_array.php');
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_is_post_revision.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_list_bookmarks.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_list_categories.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_list_pages.php');
