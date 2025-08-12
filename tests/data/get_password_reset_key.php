28fada71c0f6a6c0bd8fa1d882f05a082fbe674a
Marian <42134098+IanDelMar@users.noreply.github.com>
2025-08-12 20:49:35 +0200

    Fix impurity check for get_password_reset_key

--- tests/data/get_password_reset_key.php
+++ tests/data/get_password_reset_key.php
@@ -11,18 +11,20 @@ use function PHPStan\Testing\assertType;
  * Check impurity
  */
 
-if (is_wp_error(get_password_reset_key(Faker::wpUser()))) {
-    assertType('string|WP_Error', get_password_reset_key(Faker::wpUser()));
+$user = Faker::wpUser();
+
+if (is_wp_error(get_password_reset_key($user))) {
+    assertType('string|WP_Error', get_password_reset_key($user));
 }
 
-if (! is_wp_error(get_password_reset_key(Faker::wpUser()))) {
-    assertType('string|WP_Error', get_password_reset_key(Faker::wpUser()));
+if (! is_wp_error(get_password_reset_key($user))) {
+    assertType('string|WP_Error', get_password_reset_key($user));
 }
 
-if (is_string(get_password_reset_key(Faker::wpUser()))) {
-    assertType('string|WP_Error', get_password_reset_key(Faker::wpUser()));
+if (is_string(get_password_reset_key($user))) {
+    assertType('string|WP_Error', get_password_reset_key($user));
 }
 
-if (! is_string(get_password_reset_key(Faker::wpUser()))) {
-    assertType('string|WP_Error', get_password_reset_key(Faker::wpUser()));
+if (! is_string(get_password_reset_key($user))) {
+    assertType('string|WP_Error', get_password_reset_key($user));
 }
