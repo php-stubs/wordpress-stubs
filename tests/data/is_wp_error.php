6b244017647ce0d795c2be66a89be07006578912
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2025-08-13 02:40:53 +0200

    Update is_wp_error test to cover more cases (#347)

--- tests/data/is_wp_error.php
+++ tests/data/is_wp_error.php
@@ -9,11 +9,28 @@ use WP_Error;
 use function is_wp_error;
 use function PHPStan\Testing\assertType;
 
+/*
+ * Check return type
+ */
+
 assertType('false', is_wp_error(Faker::string()));
 assertType('true', is_wp_error(new WP_Error()));
 assertType('bool', is_wp_error(Faker::mixed()));
 
-$mixed = Faker::mixed();
-if (is_wp_error($mixed)) {
-    assertType('WP_Error', $mixed);
+/*
+ * Check type specification
+ */
+
+$stringOrWpError = Faker::union(Faker::string(), Faker::wpError());
+if (is_wp_error($stringOrWpError)) {
+    assertType('WP_Error', $stringOrWpError);
+    return;
 }
+assertType('string', $stringOrWpError);
+
+$stringOrWpError = Faker::union(Faker::string(), Faker::wpError());
+if (! is_wp_error($stringOrWpError)) {
+    assertType('string', $stringOrWpError);
+    return;
+}
+assertType('WP_Error', $stringOrWpError);
