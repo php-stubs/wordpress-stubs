694f9250183b9863709e9a2e68808ff272106886
Marian <42134098+IanDelMar@users.noreply.github.com>
2025-08-20 12:41:36 +0200

    Narrow return type of wp_update_link()
    The function [`wp_update_link()`](https://developer.wordpress.org/reference/functions/wp_update_link/) returns the result of `wp_insert_link()` with `$wp_error = true`.

--- tests/TypeInferenceTest.php
+++ tests/TypeInferenceTest.php
@@ -112,6 +112,7 @@ class TypeInferenceTest extends TypeInferenceTestCase
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_theme.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_translations.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_unique_id.php');
+        yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_update_link.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_widget_factory.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_widget_rss.php');
         yield from $this->gatherAssertTypes(__DIR__ . '/data/wp_widgets_access_body_class.php');
