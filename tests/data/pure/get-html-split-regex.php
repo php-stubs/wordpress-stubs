85be5ffcbdae6feb22451c42e3a854eae0706deb
Marian <42134098+IanDelMar@users.noreply.github.com>
2025-09-06 12:19:15 +0200

    Remove unused variable

--- tests/data/pure/get-html-split-regex.php
+++ tests/data/pure/get-html-split-regex.php
@@ -7,7 +7,6 @@ namespace PhpStubs\WordPress\Core\Tests;
 use function get_html_split_regex;
 use function PHPStan\Testing\assertType;
 
-$value = Faker::string();
 if (get_html_split_regex() === 'foo') {
     assertType("'foo'", get_html_split_regex());
 }
