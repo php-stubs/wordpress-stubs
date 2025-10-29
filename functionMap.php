13139c7d5ba382fa7137735b148010ad6849c840
Marian <42134098+IanDelMar@users.noreply.github.com>
2025-10-29 05:03:49 +0100

    Narrow $callback parameter type for add_shortcode()

--- functionMap.php
+++ functionMap.php
@@ -49,7 +49,7 @@ return [
     'add_pages_page' => [null, 'callback' => "''|callable"],
     'add_plugins_page' => [null, 'callback' => "''|callable"],
     'add_posts_page' => [null, 'callback' => "''|callable"],
-    'add_shortcode' => [null, 'tag' => 'non-empty-string'],
+    'add_shortcode' => ['void', 'tag' => 'non-empty-string', 'callback' => 'callable(array<string>, string|null, string): string'],
     'add_submenu_page' => [null, 'callback' => "''|callable"],
     'add_theme_page' => [null, 'callback' => "''|callable"],
     'add_users_page' => [null, 'callback' => "''|callable"],
--- tests/ParameterTypeTest.php
+++ tests/ParameterTypeTest.php
@@ -57,8 +57,14 @@ class ParameterTypeTest extends IntegrationTest
             [
                 ['Parameter #1 $tag of function add_shortcode expects non-empty-string, 1 given.', 10],
                 ["Parameter #1 \$tag of function add_shortcode expects non-empty-string, '' given.", 11],
-                // Maybes
                 ['Parameter #1 $tag of function add_shortcode expects non-empty-string, string given.', 14],
+                ['Parameter #2 $callback of function add_shortcode expects callable(array<string>, string|null, string): string, Closure(): void given.', 17],
+                ['Parameter #2 $callback of function add_shortcode expects callable(array<string>, string|null, string): string, Closure(string): string given.', 18],
+                ['Parameter #2 $callback of function add_shortcode expects callable(array<string>, string|null, string): string, Closure(array, string): string given.', 19],
+                ['Parameter #2 $callback of function add_shortcode expects callable(array<string>, string|null, string): string, Closure(array, string|null, bool): string given.', 20],
+                ["Parameter #2 \$callback of function add_shortcode expects callable(array<string>, string|null, string): string, 'addShortcodeVoid' given.", 21],
+                ["Parameter #2 \$callback of function add_shortcode expects callable(array<string>, string|null, string): string, 'addShortcodeIntDoc' given.", 22],
+
             ]
         );
     }
