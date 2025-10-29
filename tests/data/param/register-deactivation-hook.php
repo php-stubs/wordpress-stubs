8335bbaf29949fad1e1362921c208a386bb21d76
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2025-10-29 12:49:40 +0100

    Narrow $callback type for register_activation/deactivation/uninstall_hook() (#401)
    * Narrow $callback type for register_activation/deactivation/uninstall_hook()
    
    * Update ParameterTypeTest.php

---
+++ tests/data/param/register-deactivation-hook.php
@@ -0,0 +1,15 @@
+<?php // phpcs:disable
+
+declare(strict_types=1);
+
+use PhpStubs\WordPress\Core\Tests\Faker;
+
+$file = Faker::string();
+
+// Incorrect $callback
+register_deactivation_hook($file, static function (string $incorrect): void {}); // Incorrect parameter type
+register_deactivation_hook($file, static function (bool $correct): int {return Faker::int();}); // Incorrect return type
+
+// Correct $callback
+register_deactivation_hook($file, static function (): void {});
+register_deactivation_hook($file, static function (bool $correct): void {});
