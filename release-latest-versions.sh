1a6a24b594d25550cd29e0d83a24a9ff177ddb5a
Viktor Szépe <viktor@szepe.net>
2025-12-02 22:02:15 +0000

    Generate stubs for WordPress 6.9.0

--- release-latest-versions.sh
+++ release-latest-versions.sh
@@ -37,7 +37,8 @@ CORE_JSON="$(wget -q -O- "https://packagist.org/packages/johnpbloch/wordpress-co
 #for MINOR in                        4.7 4.8 4.9 \
 #        5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7 5.8 5.9 \
 for MINOR in \
-        6.0 6.1 6.2 6.3 6.4 6.5 6.6 6.7 6.8 6.9; do
+        6.0 6.1 6.2 6.3 6.4 6.5 6.6 6.7 6.8 6.9 \
+        7.0; do
     # Find latest version
     printf -v JQ_FILTER '.package.versions[].version | select(test("^%s\\\\.%s\\\\.\\\\d+$"))' "${MINOR%.*}" "${MINOR#*.}"
     LATEST_FIVE="$(jq -r "$JQ_FILTER" <<<"$CORE_JSON" | sort -t "." -k 3 -g | tail -n 5)"
