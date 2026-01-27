b8408ea55d7396e24ec4834004ef5a9cf470e02e
Marian <42134098+IanDelMar@users.noreply.github.com>
2026-01-27 20:14:42 +0100

    Update binary string fix for AVIF Info class
    Updates the fix to also capture the `binary string` in the return type of `Avifinfo\read()`.

--- generate.sh
+++ generate.sh
@@ -17,8 +17,8 @@ fi
 # Convert psalm-incompatible callable types in Abilities API
 sed -i -e 's#callable( mixed $input= )#callable(mixed=)#g' source/wordpress/wp-includes/abilities-api/class-wp-ability.php
 
-# Fix incorrect param type in AVIF Info class
-sed -i -e 's#@param binary string \$input#@param string $input#' source/wordpress/wp-includes/class-avif-info.php
+# Fix incorrect "binary string" type in AVIF Info class
+sed -i -e 's#@\(param\|return\) binary string#@\1 string#g' source/wordpress/wp-includes/class-avif-info.php
 
 # Exclude globals.
 "$(dirname "$0")/vendor/bin/generate-stubs" \
