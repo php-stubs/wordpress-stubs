26dc1e456d8b8444f86a18a20df1b54fde18c2ad
Marian <42134098+IanDelMar@users.noreply.github.com>
2026-02-24 17:32:29 +0100

    block_core_..._build_css_...: narrow return type

--- tests/data/return/block-core.php
+++ tests/data/return/block-core.php
@@ -2,33 +2,19 @@
 
 declare(strict_types=1);
 
-namespace PhpStubs\WordPress\Core\Tests;
-
-use function block_core_navigation_link_render_submenu_icon;
-use function block_core_navigation_render_submenu_icon;
-use function block_core_navigation_submenu_render_submenu_icon;
-use function block_core_post_time_to_read_word_count;
-use function build_dropdown_script_block_core_categories;
-use function render_block_core_archives;
-use function render_block_core_categories;
-use function render_block_core_latest_comments;
-use function render_block_core_latest_posts;
-use function render_block_core_loginout;
-use function render_block_core_query_total;
-use function render_block_core_rss;
-use function render_block_core_search;
-use function render_block_core_site_logo;
-use function render_block_core_widget_group;
+use PhpStubs\WordPress\Core\Tests\Faker;
 use function PHPStan\Testing\assertType;
-
-assertType('non-falsy-string', block_core_navigation_link_render_submenu_icon());
-assertType('non-falsy-string', block_core_navigation_render_submenu_icon());
-assertType('non-falsy-string', block_core_navigation_submenu_render_submenu_icon());
 
 assertType('int<0, max>', block_core_post_time_to_read_word_count(Faker::string(), Faker::string()));
 
 assertType('non-falsy-string', build_dropdown_script_block_core_categories(Faker::string()));
 
+// Submenu icon
+assertType('non-falsy-string', block_core_navigation_link_render_submenu_icon());
+assertType('non-falsy-string', block_core_navigation_render_submenu_icon());
+assertType('non-falsy-string', block_core_navigation_submenu_render_submenu_icon());
+
+// Render block
 assertType('non-falsy-string', render_block_core_archives(Faker::array()));
 assertType('non-falsy-string', render_block_core_categories(Faker::array(), Faker::string(), Faker::wpBlock()));
 assertType('non-falsy-string', render_block_core_latest_comments(Faker::array()));
@@ -39,3 +25,11 @@ assertType('non-falsy-string', render_block_core_rss(Faker::array()));
 assertType('non-falsy-string', render_block_core_search(Faker::array()));
 assertType('non-falsy-string', render_block_core_site_logo(Faker::array()));
 assertType('non-falsy-string', render_block_core_widget_group(Faker::array(), Faker::string(), Faker::wpBlock()));
+
+// Build CSS
+assertType('array{css_classes: list<string>, inline_styles: string}', block_core_home_link_build_css_colors(Faker::array()));
+assertType('array{css_classes: list<string>, inline_styles: string}', block_core_home_link_build_css_font_sizes(Faker::array()));
+assertType('array{css_classes: list<string>, inline_styles: string}', block_core_navigation_link_build_css_colors(Faker::array(), Faker::array()));
+assertType('array{css_classes: list<string>, inline_styles: string}', block_core_navigation_link_build_css_font_sizes(Faker::array()));
+assertType('array{css_classes: list<string>, inline_styles: string, overlay_css_classes: list<string>, overlay_inline_styles: string}', block_core_page_list_build_css_colors(Faker::array(), Faker::array()));
+assertType('array{css_classes: list<string>, inline_styles: string}', block_core_page_list_build_css_font_sizes(Faker::array()));
