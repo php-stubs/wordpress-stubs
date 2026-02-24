d02c71346aca61c0a31950d45751a1a0e7bb1f0f
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2026-02-24 10:41:39 +0100

    block_core_navigation_..._submenu_icon: narrow return type (#443)

--- tests/data/return/block-core.php
+++ tests/data/return/block-core.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace PhpStubs\WordPress\Core\Tests;
 
+use function block_core_navigation_link_render_submenu_icon;
+use function block_core_navigation_render_submenu_icon;
+use function block_core_navigation_submenu_render_submenu_icon;
 use function block_core_post_time_to_read_word_count;
 use function build_dropdown_script_block_core_categories;
 use function render_block_core_archives;
 use function render_block_core_categories;
 use function render_block_core_query_total;
 use function PHPStan\Testing\assertType;
+
+assertType('non-falsy-string', block_core_navigation_link_render_submenu_icon());
+assertType('non-falsy-string', block_core_navigation_render_submenu_icon());
+assertType('non-falsy-string', block_core_navigation_submenu_render_submenu_icon());
 
 assertType('int<0, max>', block_core_post_time_to_read_word_count(Faker::string(), Faker::string()));
 
