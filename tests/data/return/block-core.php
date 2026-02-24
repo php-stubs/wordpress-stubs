2e96d07fbf05d61a39bb21576866fa8311eec02f
Marian <42134098+IanDelMar@users.noreply.github.com>
2026-02-24 01:21:41 +0100

    Move assertions to block-core.php

--- tests/data/return/block-core.php
+++ tests/data/return/block-core.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace PhpStubs\WordPress\Core\Tests;
 
 use function build_dropdown_script_block_core_categories;
+use function render_block_core_archives;
 use function render_block_core_categories;
 use function PHPStan\Testing\assertType;
 
+assertType('non-falsy-string', render_block_core_archives(Faker::array()));
 assertType('non-falsy-string', render_block_core_categories(Faker::array(), Faker::string(), Faker::wpBlock()));
 
 assertType('non-falsy-string', build_dropdown_script_block_core_categories(Faker::string()));
