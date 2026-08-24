349dc822932e17188b546310cb7a9d24e5ccf210
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2026-08-24 20:58:59 +0200

    path_is_absolute: narrow return type and assert param type (#482)
    Co-authored-by: Viktor Szépe <viktor@szepe.net>

--- functionMap.php
+++ functionMap.php
@@ -181,6 +181,7 @@ return [
     'newblog_notify_siteadmin' => [null, 'deprecated' => "''"],
     'next_posts' => ['($display is true ? void : string)'],
     'paginate_links' => ["(\$args is array{total: int<min, 1>}&array ? void : (\$args is array{type: 'array'}&array ? list<string> : string))"],
+    'path_is_absolute' => ['($path is non-falsy-string ? bool : false)', '@phpstan-assert-if-true =non-falsy-string $path' => ''],
     'path_join' => ['non-falsy-string'],
     'post_type_archive_title' => ['($display is true ? void : string|void)'],
     'prep_atom_text_construct' => ["array{'html'|'text'|'xhtml', string}"],
